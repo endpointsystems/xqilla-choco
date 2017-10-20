51c2628dbbc3dbb5a87cf15df7e9d34083b93293
Lucas Vogel <lucas.vogel@endpointsystems.com>
2017-10-20 15:01:48 -0400

    checksum inclusion

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,5 +2,5 @@ $packageName = 'xqilla'
 $url = "https://raw.githubusercontent.com/endpointsystems/xqilla-choco/master/tools/XQilla-$version.zip"
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $version = '2.3.3'
-
-Install-ChocolateyZipPackage $packageName $url $toolsDir
+$checksum = '433F4414BCEBE39289E350543031EB80'
+Install-ChocolateyZipPackage -PackageName $packageName -Url $url -UnzipLocation $toolsDir -Checksum $checksum -ChecksumType md5
